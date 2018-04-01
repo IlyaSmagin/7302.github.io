10004f47c65a345ea1e0952b8ddb694aadefc1c4
espkk <espkk@protonmail.ch>
2018-04-01 15:08:14 +0300

    code cleanup, fixes, tests

--- sets.js
+++ sets.js
@@ -1,11 +1,9 @@
-"use strict";
+'use strict';
 
-function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return right[Symbol.hasInstance](left); } else { return left instanceof right; } }
-
-function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
+// Babel
+function _instanceof(left, right) { if (right != null && typeof Symbol !== 'undefined' && right[Symbol.hasInstance]) { return right[Symbol.hasInstance](left); } else { return left instanceof right; } }
+function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 var Utils =
@@ -16,119 +14,114 @@ var Utils =
     }
 
     _createClass(Utils, null, [{
-      key: "subU",
+      key: 'subU',
       // Superscript для Юникода
       value: function subU(n) {
-        var supArr = ["⁰", "¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹"];
-        if (n == 0) return supArr[0];
-        var minus = false;
+        var supArr = ['⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹'];
+        if (n == 0)
+          return supArr[0];
 
+        var minus = false;
         if (n < 0) {
           minus = true;
           n = -n;
         }
 
-        var result = "";
-
+        var result = '';
         while (n > 0) {
           var rem = n % 10;
           result = supArr[rem] + result;
           n = ~~(n / 10);
         }
 
-        return minus ? "⁻" + result : result;
+        return minus ? '⁻' + result : result;
       }
     }]);
 
     return Utils;
   }();
+
 /*******************************************/
-
 /**************Классы множеств**************/
-
 /*******************************************/
 // Множество натуральных чисел с 0
-
-
 var Natural =
   /*#__PURE__*/
   function () {
     function Natural(val) {
       _classCallCheck(this, Natural);
-
       this.a = val;
     }
 
     _createClass(Natural, [{
-      key: "delLeadingZeros",
+      key: 'delLeadingZeros',
       value: function delLeadingZeros() {
-        while (this.arr.length > 1 && this.arr[0] == 0) {
+        while (this.arr.length > 1 && this.arr[0] == 0)
           this.arr.shift();
-        }
       }
     }, {
-      key: "a",
+      key: 'a',
       get: function get() {
         return this.arr;
       },
       set: function set(val) {
-        if (!Array.isArray(val)) val = val.toString().split("");
-        this.arr = val.map(function (digit) {
-          return Number(digit);
-        });
-        if (!/^\d+$/.test(this.arr.join(""))) debugger;
+        if (!Array.isArray(val))
+          val = val.toString().split('');
+        this.arr = val.map(function (digit) { return Number(digit); });
+        if (!/^\d+$/.test(this.arr.join('')))
+          throw new Error('Invalid value for Natural');
       }
     }, {
-      key: "n",
+      key: 'n',
       get: function get() {
-        if (this.arr) for (var i = 0; i < this.arr.length; i++) {
-          if (this.arr[i] > 0) return this.arr.length - i;
-        }
+        if (this.arr) for (var i = 0; i < this.arr.length; i++)
+          if (this.arr[i] > 0)
+            return this.arr.length - i;
         return 0;
       }
     }]);
 
     return Natural;
   }();
+Natural.prototype.toString = function () { return this.a.join(''); };
+Natural.prototype.valueOf = Natural.prototype.toString;
 
-Natural.prototype.toString = function () {
-  return this.a.join("");
-};
-
-Natural.prototype.valueOf = Natural.prototype.toString; // Множество целых чисел
-
+// Множество целых чисел
 var Integer =
   /*#__PURE__*/
   function () {
     function Integer(val) {
       _classCallCheck(this, Integer);
-
       this.a = val;
     }
 
     _createClass(Integer, [{
-      key: "a",
+      key: 'a',
       get: function get() {
         return this.natural.a;
       },
       set: function set(val) {
-        if (!Array.isArray(val)) val = val.toString().split("");
-        if (!/^-?\d+$/.test(val.join(""))) debugger;
+        if (!Array.isArray(val))
+          val = val.toString().split('');
+        if (!/^-?\d+$/.test(val.join('')))
+         throw new Error('Invalid value for Integer');
 
-        if (val[0] === "-") {
+        if (val[0] === '-') {
           this.negative = true;
           val.shift();
-        } else this.negative = false;
+        }
+        else
+          this.negative = false;
 
         this.natural = new Natural(val);
       }
     }, {
-      key: "n",
+      key: 'n',
       get: function get() {
         return this.natural.n;
       }
     }, {
-      key: "b",
+      key: 'b',
       get: function get() {
         return this.negative;
       },
@@ -139,51 +132,48 @@ var Integer =
 
     return Integer;
   }();
+Integer.prototype.toString = function () { return (this.b ? '-' : '') + this.natural; };
+Integer.prototype.valueOf = Integer.prototype.toString;
 
-Integer.prototype.toString = function () {
-  return (this.b ? "-" : "") + this.natural;
-};
-
-Integer.prototype.valueOf = Integer.prototype.toString; // Множество рациональных чисел
-
+// Множество рациональных чисел
 var Rational =
   /*#__PURE__*/
   function () {
     function Rational(numerator, denumerator) {
       _classCallCheck(this, Rational);
 
-      var arr = numerator.toString().split("/");
-
+      var arr = numerator.toString().split('/');
       if (arr.length > 1) {
         numerator = arr[0];
         denumerator = arr[1];
       }
 
-      this.p = numerator;
-      this.q = denumerator;
-    } // Числитель
+      this.p = numerator; // Числитель
+      this.q = denumerator; // Знаменатель
+    }
 
 
     _createClass(Rational, [{
-      key: "p",
+      key: 'p',
       get: function get() {
         return this.numerator;
-      } // Знаменатель
+      }
       ,
       set: function set(val) {
         this.numerator = new Integer(val);
       }
     }, {
-      key: "q",
+      key: 'q',
       get: function get() {
         return this.denumerator;
       },
       set: function set(val) {
-        if (val === undefined) val = 1;
+        if (val === undefined)
+          val = 1;
         this.denumerator = new Natural(val);
       }
     }, {
-      key: "b",
+      key: 'b',
       get: function get() {
         return this.numerator.b;
       }
@@ -191,38 +181,37 @@ var Rational =
 
     return Rational;
   }();
-
 Rational.prototype.toString = function () {
   var str = this.numerator;
-  if (this.denumerator !== undefined) str += "/" + this.denumerator;
+  if (this.denumerator !== undefined)
+    str += '/' + this.denumerator;
   return str;
 };
-
 Rational.prototype.valueOf = Rational.prototype.toString;
 
+// Многочлен
 var Polynomial =
   /*#__PURE__*/
   function () {
     function Polynomial(val) {
       _classCallCheck(this, Polynomial);
-
       this.c = val;
     }
 
     _createClass(Polynomial, [{
-      key: "c",
+      key: 'c',
       get: function get() {
         return this.arr;
       },
       set: function set(val) {
-        if (Array.isArray(val)) this.arr = val;else this.arr = val.toString().split(" ");
-
+        if (Array.isArray(val))
+          this.arr = val;else this.arr = val.toString().split(' ');
         for (var i = 0; i < this.arr.length; i++) {
           this.arr[i] = new Rational(this.arr[i]);
         }
       }
     }, {
-      key: "m",
+      key: 'm',
       get: function get() {
         return this.arr.length - 1;
       }
@@ -230,32 +219,32 @@ var Polynomial =
 
     return Polynomial;
   }();
-
 Polynomial.prototype.toString = function () {
   // TODO: use big number arithmetics
   function intRat(rat) {
-    if (rat.q.m == 1 && rat.q.a[0] == 1) return rat.p.a.join("");
-    return rat.p.a.join("") % rat.q.a.join("") ? null : rat.p.a.join("") / rat.q.a.join("");
+    if (rat.q.m == 1 && rat.q.a[0] == 1)
+      return rat.p.a.join('');
+    return rat.p.a.join('') % rat.q.a.join('') ? null : rat.p.a.join('') / rat.q.a.join('');
   }
 
   function formatRat(rat) {
     var result = intRat(rat);
-    if (result === null) result = rat;
+    if (result === null)
+      result = rat;
     return result.toString();
   }
 
-  var str = "";
-
+  var str = '';
   for (var i = 0; i < this.m; i++) {
     if (this.c[i].p.n != 0) {
       var k = formatRat(this.c[i]);
-      str += (str.length > 0 && !this.c[i].b > 0 ? "+" : "") + (k == 1 ? "" : k) + "x" + Utils.subU(this.m - i);
+      str += (str.length > 0 && !this.c[i].b > 0 ? '+' : '') + (k == 1 ? '' : k) + 'x' + Utils.subU(this.m - i);
     }
   }
 
   var constant = this.c[this.m];
-  if (constant.p.n != 0 || str.length == 0) str += (str.length > 0 && !constant.b ? "+" : "") + formatRat(constant);
+  if (constant.p.n != 0 || str.length == 0)
+    str += (str.length > 0 && !constant.b ? '+' : '') + formatRat(constant);
   return str;
 };
-
 Polynomial.prototype.valueOf = Polynomial.prototype.toString;
