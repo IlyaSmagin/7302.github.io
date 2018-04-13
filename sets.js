021c0641aa6bb6f066a4c4d7f62c3f2456fcb8a4
espkk <espkk@protonmail.ch>
2018-04-14 01:53:52 +0300

    resolve merge

--- sets.js
+++ sets.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// Babel
 function _instanceof(left, right) { if (right != null && typeof Symbol !== 'undefined' && right[Symbol.hasInstance]) { return right[Symbol.hasInstance](left); } else { return left instanceof right; } }
 function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
